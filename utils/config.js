277903db3d7541ba623af7ce937b7436067e7fa2
heyong <1434046484@qq.com>
2021-12-22 10:56:59 +0800

    first commit

---
+++ utils/config.js
@@ -0,0 +1,38 @@
+/*
+ * 
+ * 微慕小程序开源版
+ * author: jianbo
+ * organization: 微慕  www.minapper.com
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * 开源协议：MIT
+ * Copyright (c) 2017  微慕 https://www.minapper.com All rights reserved.
+ */
+
+
+
+//配置域名,域名只修改此处。
+//如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
+var DOMAIN = "www.louishe.com";
+var WEBSITENAME="听可科技|TMC"; //网站名称
+var PAGECOUNT='10'; //每页文章数目
+
+//是否启用小程序扫描二维码登录网站  true 启用  false  不启用
+//未安装微慕登录插件不要启用
+const enableScanLogin =false
+//////////////////////////////////////////////////////
+
+
+//微慕小程序端版本,请勿修改
+const minapperVersion=4.12
+const minapperSource="free"
+//////////////////////////////////////////////////////
+
+export default {
+  getDomain: DOMAIN,
+  getWebsiteName: WEBSITENAME,  
+  getPageCount: PAGECOUNT,
+  enableScanLogin,
+  minapperVersion,
+  minapperSource
+}
